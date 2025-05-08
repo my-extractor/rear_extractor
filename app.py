2ad8ab4581345cbf1e08438af03da7c3580ffccb
my-extractor <jskai244@naver.com>
2025-05-08 17:16:34 +0900

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, send_file
 import os
 import uuid
-import moviepy.editor as mp
+from moviepy import editor as mp
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
